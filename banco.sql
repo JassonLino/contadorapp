cd5d690f24645c160e402200da713b3160763da1
aluno <aluno>
2019-09-18 11:01:15 -0300

    banco de dados

---
+++ banco.sql
@@ -0,0 +1,12 @@
+
+create database  if not exists  Contadores;
+use Contadores; 
+CREATE TABLE if not exists Contador
+(
+PNome varchar(15) NOT NUll,
+Numero int(10),
+Codigo int(10) not null 
+
+);
+select * from contador;
+desc contador;
